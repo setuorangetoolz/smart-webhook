32a0fc96f02dc4e8a7998db7d55f7889f60e10fa
Sifat Sabbir <sifat@orangetoolz.com>
2022-10-29 10:58:28 +0600

    span color add

--- src/components/NewDrawer.js
+++ src/components/NewDrawer.js
@@ -1,15 +1,6 @@
 import * as React from 'react';
-import Box from '@mui/material/Box';
 import Drawer from '@mui/material/Drawer';
 import Button from '@mui/material/Button';
-import List from '@mui/material/List';
-import Divider from '@mui/material/Divider';
-import ListItem from '@mui/material/ListItem';
-import ListItemButton from '@mui/material/ListItemButton';
-import ListItemIcon from '@mui/material/ListItemIcon';
-import ListItemText from '@mui/material/ListItemText';
-import InboxIcon from '@mui/icons-material/MoveToInbox';
-import MailIcon from '@mui/icons-material/Mail';
 import SmartHooks from './SmartHooks';
 import "./smarthooks.css";
 
@@ -32,7 +23,7 @@ export default function NewDrawer() {
     <div>
       
         <React.Fragment key={anchor}>
-          <Button onClick={toggleDrawer(anchor, true)}>{anchor}</Button>
+          <Button onClick={toggleDrawer(anchor, true)}>Right</Button>
           <Drawer
             anchor={anchor}
             open={state[anchor]}
